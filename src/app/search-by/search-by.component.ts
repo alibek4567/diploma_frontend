8036b1fdf86c239f56f099dd3dd7c4bfd2024af6
ol.ilyassov <ol.ilyassov@gmail.com>
2022-05-20 04:52:31 +0600

    Dynamic Subject Set by Schedule + Time Line

--- src/app/search-by/search-by.component.ts
+++ src/app/search-by/search-by.component.ts
@@ -14,25 +14,32 @@ Chart.register(BarElement, BarController, CategoryScale, LinearScale, Tooltip, C
 })
 export class SearchByComponent implements OnInit {
 
+  // Personal Info - Auth
+  profileInfo: any
+
   username: any
   email: any
   id: any
   department: any
 
+  // Search Bar
   search: false
 
+  // Schedule parameters
   allTimeTables: any
-  timeArray = [8,9,10,11,12,13,14,15,16,17,18]
-  d1: any
-  d2: any
-  d3: any
-  d4: any
-  d5: any  
+  timeArray = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+  d1: any // Monday
+  d2: any // Tuesday
+  d3: any // Wednesday
+  d4: any // Thursday
+  d5: any // Friday
+  d6: any // Saturday
 
-  profileInfo: any
+  lessonDuration: number = 50
 
+  // Language
   languages = new Map<string, string>([
-    ["kz", "Kazaksha"],
+    ["kz", "Қазақша"],
     ["en", "English"],
     ["ru", "Русский"]
   ])
@@ -40,24 +47,59 @@ export class SearchByComponent implements OnInit {
   apiResponse: string
   
   constructor(private api: ApiCallerService, public renderer: Renderer2, private msalService: MsalService, private httpClient: HttpClient) {
-    var response = this.api.sendGetRequest("/getTimetable/1")
+    
+    var response = this.api.sendGetRequest("/timetable/group/1")
     response.subscribe(data => {
-
       const timeTables = JSON.parse(JSON.stringify(data))
+
       this.d1 = timeTables.payload["d1"]
       this.d2 = timeTables.payload["d2"]
       this.d3 = timeTables.payload["d3"]
       this.d4 = timeTables.payload["d4"]
       this.d5 = timeTables.payload["d5"]
+      this.d5 = timeTables.payload["d6"]
+
+      this.setSubjects(this.d1)
+      this.setSubjects(this.d2)
+      this.setSubjects(this.d3)
+      this.setSubjects(this.d4)
+      this.setSubjects(this.d5)
+      this.setSubjects(this.d6)
+
       this.updateHtml(this.d1)
       this.updateHtml(this.d2)
       this.updateHtml(this.d3)
       this.updateHtml(this.d4)
       this.updateHtml(this.d5)
+      this.updateHtml(this.d6)
+    }, error => { console.log(error) })
 
-    }, error => { })
-   
-   }
+    // Work of Time Line
+    window.addEventListener('load', function () {
+
+      var dateTime = getTodaysDate(+6) // Todays Date and Time with Timezone +6
+
+      var dateTimeArray = dateTime.split(',');
+
+      var dayOfTheWeek = parseInt(dateTimeArray[0])
+      var date = dateTimeArray[1]
+      var time = dateTimeArray[2].split(':')
+      var hours = parseInt(time[0])
+      var minutes = parseInt(time[1]) / 60
+      var final = 0
+      if (hours < 8) {
+        setVerticalLocation("d" + dayOfTheWeek + "_line", final)
+      } else if (hours > 21) {
+        hours = 22
+        minutes = 0
+        final = ((0.1 * (hours - 8)) + ((hours - 8) * 5) + minutes * 5)
+        setVerticalLocation("d" + dayOfTheWeek + "_line", final)
+      } else {
+        final = ((0.1 * (hours - 8)) + ((hours - 8) * 5) + minutes * 5)
+        setVerticalLocation("d" + dayOfTheWeek + "_line", final)
+      }
+    })
+  }
 
   ngOnInit(): void {
     this.msalService.instance.handleRedirectPromise().then(
@@ -106,14 +148,57 @@ export class SearchByComponent implements OnInit {
   }
 
   updateHtml(data: any) {
-    if(data!=null){
-      for(let item of data){
+    if (data != null) {
+      for (let item of data) {
         let el = (<HTMLElement>document.getElementById(item.classtime_day+"_"+(item.classtime_time).split(':')[0]));
         el.className = "block";
-        el.insertAdjacentHTML("beforeend", "<p style='margin: 0 0 0.5rem 0;; padding: 0; font-size:0.9rem'>"+item.subject+"</p>");
-        el.insertAdjacentHTML("beforeend", "<p style='margin: 0 0 0.5rem 0;; padding: 0; font-size:0.8rem'>"+item.tutor+"</p>")
-        el.insertAdjacentHTML("beforeend", "<p style='margin: 0 0 0.5rem 0;; padding: 0; font-size:0.8rem'>"+item.room+"</p>");
+        el.insertAdjacentHTML("beforeend", "<p style='margin: 0 0 0.2rem 0;; padding: 0.2rem 0 0 0; font-size:0.8rem'>"+item.subject+"</p>");
+        // el.insertAdjacentHTML("beforeend", "<p style='margin: 0 0 0.5rem 0;; padding: 0; font-size:0.8rem'>"+item.tutor+"</p>")
+        el.insertAdjacentHTML("beforeend", "<p style='margin: 0 0 0.2rem 0;; padding: 0 0 0.2rem 0; font-size:0.7rem'>"+item.room+"</p>");
       }
     }
   }
+
+  setSubjects(data: any) {
+    if (data != null) {
+      for (let item of data) {
+        let el = (<HTMLElement>document.getElementById(item.classtime_day+"_"+(item.classtime_time).split(':')[0]));
+        el.style.position = "absolute"
+
+        // height = 5rem = 60 min = 100%
+        // height = 4.16666667rem = 50 min
+        el.style.height = (this.lessonDuration * 5 / 60) + "rem"
+        
+        var timer = item.classtime_time.split(':');
+        // Get Subject Time + Time starts from 8:00
+        var hours = parseInt(timer[0]) - 8 
+        var minutes = parseInt(timer[1]) / 60
+        // Displacement = (Borders) + (Full Hours) + (Minutes)
+        var final = ((0.1 * hours) + (hours * 5) + minutes * 5) + "rem"
+
+        el.style.top = final
+        el.style.left = "0.1rem";
+
+        // Выставление ширины блока в зависимости от ширины колонны
+        // let element = <HTMLElement>document.getElementById('test');
+        // let style = window.getComputedStyle(element);
+        // let width = style.getPropertyValue('width');
+        // // console.log(width);
+        // el.style.width = width;
+      }
+    }
+  }
+}
+
+function getTodaysDate(offset: number): string {
+  var date = new Date();
+  var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+  var nd = new Date(utc + (3600000 * offset));
+  return nd.getDay() + ", " + nd.toLocaleString();
+}
+
+function setVerticalLocation(elementId: string, number: number) {
+  let element = <HTMLElement>document.getElementById(elementId);
+  element.style.display = "block";
+  element.style.top = number + "rem"
 }
