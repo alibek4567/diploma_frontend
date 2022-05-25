751ca7060579080aee2c9a3a9a4d5132c5efdac1
ol.ilyassov <ol.ilyassov@gmail.com>
2022-05-25 23:24:33 +0600

    Cleaning

--- src/app/search-by/search-by.component.ts
+++ src/app/search-by/search-by.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
-// import { ElementRef, ViewChild} from '@angular/core';
 import { ApiCallerService } from '../api-caller.service';
 import { MsalService } from '@azure/msal-angular';
-// import { AuthenticationResult } from '@azure/msal-browser';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material/dialog'
 import { SubjectPopUpComponent } from '../subject-pop-up/subject-pop-up.component';
 import { Subject } from '../subject'
-// import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-search-by',
@@ -22,75 +19,63 @@ export class SearchByComponent implements OnInit {
 
   // Search Bar
   search: boolean = false
+  searchStart: boolean = false
+  searchMode: string = 'by-group'
   searchValue: string = ''
   searchResult: string = ''
-  searchMode: string = 'by-group'
+  searchSuccess: boolean = false
 
   // Search by cabinet
   cabinet: boolean = false
 
   // Schedule parameters
-  scheduleStartTime: string = "22:00"
-  scheduleEndTime: string = "8:00"
-  timeArray: string[] = [
-    "8:00", "8:30", "9:00", "9:30",
-    "10:00", "10:30", "11:00", "11:30",
-    "12:00", "12:30", "13:00", "13:30", 
-    "14:00", "14:30", "15:00", "15:30",
-    "16:00", "16:30", "17:00", "17:30",
-    "18:00", "18:30", "19:00", "19:30",
-    "20:00", "20:30", "21:00", "21:30"
-  ]
+  scheduleStartTime: string = "24:00" //"22:00"
+  scheduleEndTime: string = "00:00" //"8:00"
+  timeArray: string[] = []
 
   // d1 = MON; d2 = TUS; d3 = WED; d4 = THU; d5 = FRI; d6 = SAT
   weekSchedule = new Map<string, Map<string, Subject[]>>()
-
-  // timeMap: Map<string, object[]>
 
   lessonDuration: number = 50
   
   constructor(private api: ApiCallerService, public renderer: Renderer2, 
     private msalService: MsalService, private httpClient: HttpClient, private dialogRef: MatDialog) {
 
-    // Current Week value
+    // Current Week Date
     let temp = getTodaysDate(+6).split(',')[1].trim()
     let date = temp.split('.')
     let dateInFormat = date[2] + '/' + date[1] + '/' + date[0]
     let time = getTodaysDate(+6).split(',')[2].trim()
     this.weekDates = getWeekDates(dateInFormat + ' ' + time)
-
-    // console.log(compareTime('24:00', '14:00'))
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   setSearch() {
-    //console.log(this.searchMode)
     if (this.searchValue == '') {
       console.log(this.searchValue)
     } else {
-      this.search = true
-
+      this.searchStart = true
       this.searchResult = this.searchValue
       switch(this.searchMode) { 
         case 'by-group': { 
-          //this.cabinet = false
+          this.cabinet = false
           this.getSearchResult("/timetable/group/" + this.searchValue)
           break; 
         }
         case 'by-teacher': { 
-          //this.cabinet = false
+          this.cabinet = false
           this.getSearchResult("/timetable/tutor/" + this.searchValue)
           break; 
         }
         case 'by-cabinet': { 
-          //this.cabinet = true
+          this.cabinet = true
           this.getSearchResult("/timetable/room/" + this.searchValue)
           // Booking part to add
           break; 
        } 
         default: { 
-           console.log("ERROR")
+           console.log("no correct search Mode")
         } 
       } 
     }
@@ -98,20 +83,15 @@ export class SearchByComponent implements OnInit {
 
   getSearchResult(request: string) {
     this.weekSchedule.clear()
-    
-    waitForElm('.grid-container').then(async () => {
-      this.setTimeline()
-      setInterval(this.setTimeline, 60 * 1000);
-    });
      
     var response = this.api.sendGetRequest(request)
     response.subscribe(data => {
+      this.searchSuccess = false
       const schedule = JSON.parse(JSON.stringify(data))
       // Sorting
       for (let i = 1; i < 7; i++) {
         if (schedule.payload["d" + i] != null) {
           for (let item of schedule.payload["d" + i]) {
-
             if (this.weekSchedule.get("d" + i) != undefined) {
               if (this.weekSchedule.get("d" + i)?.get(item.classtime_time) != undefined) {
                 this.weekSchedule.get("d" + i)?.get(item.classtime_time)?.push(item)
@@ -126,48 +106,45 @@ export class SearchByComponent implements OnInit {
         }
       }
 
-      // Compare time -----
+      // Time Table cut
       for (let i = 1; i < 7; i++) {
         if (this.weekSchedule.get("d" + i) != undefined) {
           let times = this.weekSchedule.get("d" + i)?.keys()
           if (times != undefined) {
             for (let time of times) {
-              let result = compareTime(time, this.scheduleEndTime)
-              if (result == 1) {
+              if (compareTime(time, this.scheduleEndTime) == 1) {
                 this.scheduleEndTime = time
               }
-              result = compareTime(time, this.scheduleStartTime)
-              if (result == -1) {
+              if (compareTime(time, this.scheduleStartTime) == -1) {
                 this.scheduleStartTime = time
               }
             }
           }
         }
       }
-      console.log(this.scheduleStartTime, this.scheduleEndTime)
 
-      // Time Array Values:
+      // Drawing Time blocks
+      let myArray: string[] = []
+      let start = parseInt(this.scheduleStartTime.split(':')[0])
+      let end = parseInt(this.scheduleEndTime.split(':')[0]) + 2
+      for (let i = start; i < end; i++) {
+        myArray.push(i + ':00')
+        myArray.push(i + ':30')
+      }
+      this.timeArray = myArray
 
-      // this.scheduleStartTime
-      // this.scheduleEndTime
-
-      //this.timeArray = []
-      // let myArray = []
-      // for (let i = parseInt(this.scheduleStartTime.split(':')[0]); i++; i< (parseInt(this.scheduleEndTime.split(':')[0]) + 2)) {
-      //   myArray.push(i + ':00')
-      //   myArray.push(i + ':30')
-      // }
-      // console.log(myArray)
-
-      // ------------------
-
-      // Drawing
+      // Drawing Subjects
       waitForElm('.grid-container').then(async () => {
         for (let i = 1; i < 7; i++) {
           await this.setSubjects("d" + i, this.weekSchedule.get("d" + i))
         }
       });
-    }, error => { console.log(error) })
+    }, error => { 
+      this.searchSuccess = false
+      console.log(error) 
+      this.searchResult = error.statusText
+      this.timeArray = [ ]
+    })
   }
 
   async setSubjects(day: string, daySchedule: any) {
@@ -247,34 +224,6 @@ export class SearchByComponent implements OnInit {
     return formDate.slice(3, 10) + ' | ' + formDate.slice(0, 3)
   }
 
-  setTimeline() {
-    // Work of Time Line
-    var dateTime = getTodaysDate(+6).split(',') // Todays Date and Time with Timezone +6
-
-    //var dateTimeArray = dateTime.split(',');
-
-    var dayOfTheWeek = parseInt(dateTime[0])
-    //var date = dateTimeArray[1]
-    var time = dateTime[2].split(':')
-    var hours = parseInt(time[0])
-    var minutes = parseInt(time[1]) / 60
-    var final = 0
-    if (hours < 8) {
-      setVerticalLocation("d" + dayOfTheWeek + "_line", final)
-      setHeightHTML("d" + dayOfTheWeek + "_cover-column", final)
-    } else if (hours > 21) {
-      hours = 22
-      minutes = 0
-      final = ((0.1 * (hours - 8)) + ((hours - 8) * 5) + minutes * 5)
-      setVerticalLocation("d" + dayOfTheWeek + "_line", final)
-      setHeightHTML("d" + dayOfTheWeek + "_cover-column", final)
-    } else {
-      final = ((0.1 * (hours - 8)) + ((hours - 8) * 5) + minutes * 5)
-      setVerticalLocation("d" + dayOfTheWeek + "_line", final)
-      setHeightHTML("d" + dayOfTheWeek + "_cover-column", final)
-    }
-  }
-
   // HTML interface functions
   formatDayAndWeek(date: string | undefined): string {
     if (date == undefined) {
@@ -320,18 +269,6 @@ function getWeekDates(date: string): Map<string, string> {
     weekDates.set('d' + (i + 1), new Date(currentDay.setDate(monday + i - 1)).toLocaleString()) // toUTCString()
   }
   return weekDates
-}
-
-function setVerticalLocation(elementId: string, number: number) {
-  let element = <HTMLElement>document.getElementById(elementId);
-  element.style.display = "block";
-  element.style.top = number + "rem"
-}
-
-function setHeightHTML(elementId: string, number: number) {
-  let element = <HTMLElement>document.getElementById(elementId);
-  element.style.display = "block";
-  element.style.height = number + "rem"
 }
 
 function compareTime(time1: string, time2: string): number {
